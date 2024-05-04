daabc3300bf4354762e3ea9153ce45d2c377f72e
Luiggi Antonio Loayza <U202011431@upc.edu.pe>
2024-05-04 11:27:07 -0500

    feat: se agregó routines.vue

--- src/trainease-customers/pages/routines.vue
+++ src/trainease-customers/pages/routines.vue
@@ -1,13 +1,13 @@
 
 <template>
-    <div class="container">
-      <ToolbarComponent />
-      <!-- Asegúrate de que estás usando v-for para pasar cada trainer a TrainerCard -->
-      <ExerciseCard
-        v-for="exercise in exercises"
-        :key="exercise.id"
-        :exercise="exercise" />
-    </div>
+  <div class="container">
+    <ToolbarComponent />
+    <!-- Asegúrate de que estás usando v-for para pasar cada trainer a TrainerCard -->
+    <ExerciseCard
+      v-for="exercise in exercises"
+      :key="exercise.id"
+      :exercise="exercise" />
+  </div>
 </template>
 
 <script>
@@ -16,27 +16,27 @@ import ExerciseCard from '../components/exercisecard.component.vue';
 import ExerciseService from '../services/exerciseinfo.js'; 
 
 export default {
-  components: {
-    ToolbarComponent,
-    ExerciseCard
-  },
-  data() {
-    return {
-      exercises: []
-    };
-  },
-  async created() {
-    try {
-      this.exercises = await ExerciseService.getAllExercises();
-      console.log('Datos de exercises:', this.exercise);
-    } catch (error) {
-      console.error('Error fetching exercises:', error);
-    }
+components: {
+  ToolbarComponent,
+  ExerciseCard
+},
+data() {
+  return {
+    exercises: []
+  };
+},
+async created() {
+  try {
+    this.exercises = await ExerciseService.getAllExercises();
+    console.log('Datos de exercises:', this.exercise);
+  } catch (error) {
+    console.error('Error fetching exercises:', error);
   }
-  
+}
+
 }
 </script>
-  
+
 <style>
 .container {
 background-color: #ffdc7a; /* Fondo negro para el contenedor */
